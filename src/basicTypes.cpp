06fabe865725e78c5468323460e01f2b0dd30faf
drm <dave@millermattson.com>
2021-12-11 17:37:56 -0800

    optimize basic types, formatting

--- src/basicTypes.cpp
+++ src/basicTypes.cpp
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include "basicTypes.h"
+
 namespace BS {
 
 // This rotates a Dir value by the specified number of steps. There are
@@ -14,7 +15,7 @@ constexpr Compass SE = BS::Compass::SE; constexpr Compass S = BS::Compass::S;
 constexpr Compass SW = BS::Compass::SW; constexpr Compass W = BS::Compass::W;
 constexpr Compass C = BS::Compass::CENTER;
 
-const Dir rotations[72] = {SW, W, NW, N, NE, E, SE, S,
+const Dir rotations[72] = { SW, W, NW, N, NE, E, SE, S,
                             S, SW, W, NW, N, NE, E, SE,
                             SE, S, SW, W, NW, N, NE, E,
                             W, NW, N, NE, E, SE, S, SW,
@@ -22,19 +23,13 @@ const Dir rotations[72] = {SW, W, NW, N, NE, E, SE, S,
                             E, SE, S, SW, W, NW, N, NE,
                             NW, N, NE, E, SE, S, SW, W,
                             N, NE, E, SE, S, SW, W, NW,
-                            NE, E, SE, S, SW, W, NW, N};
-
-const Coord NormalisedCoords[9] = {Coord(-1,-1), Coord(0,-1), Coord(1,-1),
-                                    Coord(-1,0), Coord(0,0), Coord(1,0),
-                                    Coord(-1,1), Coord(0,1), Coord(1,1)};
-
-const Dir conversion[16] {S, C, SW, N, SE, E, N,
-                            N, N, N, W, NW, N, NE, N, N};
+                            NE, E, SE, S, SW, W, NW, N };
 
 Dir Dir::rotate(int n) const
 {
     return rotations[asInt() * 8 + (n & 7)];
 }
+
 
 /*
     A normalized Coord is a Coord with x and y == -1, 0, or 1.
@@ -45,26 +40,40 @@ Dir Dir::rotate(int n) const
 
        Coord { (d%3) - 1, (trunc)(d/3) - 1  }
 
-       0 => -1, -1
-       1 =>  0, -1
-       2 =>  1, -1,
-       3 => -1,  0
-       4 =>  0,  0
-       5 =>  1   0
-       6 => -1,  1
-       7 =>  0,  1
-       8 =>  1,  1
+       0 => -1, -1  SW
+       1 =>  0, -1  S
+       2 =>  1, -1, SE
+       3 => -1,  0  W
+       4 =>  0,  0  CENTER
+       5 =>  1   0  E
+       6 => -1,  1  NW
+       7 =>  0,  1  N
+       8 =>  1,  1  NE
 */
+const Coord NormalizedCoords[9] = { 
+    Coord(-1,-1), // SW
+    Coord(0,-1),  // S
+    Coord(1,-1),  // SE
+    Coord(-1,0),  // W
+    Coord(0,0),   // CENTER
+    Coord(1,0),   // E
+    Coord(-1,1),  // NW
+    Coord(0,1),   // N
+    Coord(1,1)    // NE
+};
+
 
 Coord Dir::asNormalizedCoord() const
 {
-    return NormalisedCoords[asInt()];
+    return NormalizedCoords[asInt()];
 }
+
 
 Polar Dir::asNormalizedPolar() const
 {
     return Polar{1, dir9};
 }
+
 
 /*
     A normalized Coord has x and y == -1, 0, or 1.
@@ -77,9 +86,11 @@ Coord Coord::normalize() const
     return asDir().asNormalizedCoord();
 }
 
+
 // Effectively, we want to check if a coordinate lies in a 45 degree region (22.5 degrees each side)
-// centred on each compass direction. By first rotating the system by 22.5 degrees clockwise
+// centered on each compass direction. By first rotating the system by 22.5 degrees clockwise
 // the boundaries to these regions become much easier to work with as they just align with the 8 axes.
+// (Thanks to @Asa-Hopkins for this optimization -- drm)
 Dir Coord::asDir() const
 {
     // tanN/tanD is the best rational approximation to tan(22.5) under the constraint that
@@ -88,13 +99,16 @@ Dir Coord::asDir() const
     // the closest a pair of int16_t's come to any of these lines is 8e-8 degrees, so the result is exact
     constexpr uint16_t tanN = 13860;
     constexpr uint16_t tanD = 33461;
+    const Dir conversion[16] {  S, C, SW, N, SE, E, N,
+                                N, N, N, W, NW, N, NE, N, N};
 
     const int32_t xp = x * tanD + y * tanN;
     const int32_t yp = y * tanD - x * tanN;
     // We can easily check which side of the four boundary lines
     // the point now falls on, giving 16 cases, though only 9 are
     // possible.
-    return conversion[(yp > 0)*8 + (xp>0)*4 + (yp>xp)*2 + (yp>=-xp)];
+
+    return conversion[(yp > 0) * 8 + (xp > 0) * 4 + (yp > xp) * 2 + (yp >= -xp)];
 }
 
 
@@ -102,6 +116,7 @@ Polar Coord::asPolar() const
 {
     return Polar{(int)length(), asDir()};
 }
+
 
 /*
     Compass values:
@@ -112,8 +127,17 @@ Polar Coord::asPolar() const
 */
 Coord Polar::asCoord() const
 {   //3037000500 is 1/sqrt(2) in 32.32 fixed point
-    constexpr int64_t coordMags[9] = {3037000500, 1L << 32, 3037000500, 1L << 32,
-                                    0, 1L << 32, 3037000500, 1L << 32, 3037000500};
+    constexpr int64_t coordMags[9] = {
+        3037000500,  // SW
+        1L << 32,    // S
+        3037000500,  // SE
+        1L << 32,    // W
+        0,           // CENTER
+        1L << 32,    // E
+        3037000500,  // NW
+        1L << 32,    // N
+        3037000500   // NE
+    };
 
     int64_t len = (coordMags[dir.asInt()]*mag);
 
@@ -125,21 +149,23 @@ Coord Polar::asCoord() const
 
     int64_t temp = ((int64_t)mag >> 32) ^ ((1L << 31) - 1);
     len = (len + temp)/(1L << 32); //Divide to make sure we get an arithmetic shift
-    return NormalisedCoords[dir.asInt()] * len;
+
+    return NormalizedCoords[dir.asInt()] * len;
 }
 
 
 // returns -1.0 (opposite directions) .. +1.0 (same direction)
 // returns 1.0 if either vector is (0,0)
-
 float Coord::raySameness(Coord other) const
 {
     int64_t mag = ((int64_t)x * x + y * y) * (other.x * other.x + other.y * other.y);
     if (mag == 0) {
         return 1.0; // anything is "same" as zero vector
     }
-    return (x * other.x + y * other.y)/std::sqrt(mag);
+
+    return (x * other.x + y * other.y) / std::sqrt(mag);
 }
+
 
 // returns -1.0 (opposite directions) .. +1.0 (same direction)
 // returns 1.0 if self is (0,0) or d is CENTER
