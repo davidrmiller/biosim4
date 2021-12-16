0bcc5fb350f01ea46473b0c9ca7a141970b7ac0c
David R. Miller <davidrmiller@users.noreply.github.com>
2021-12-16 08:05:58 -0800

    Optimize basic types (#40) Fixes #29
    * Clean up most functions
    
    * Fix compile error
    
    * Fix potential overflow in `raySameness`
    
    * Replace `#define`
    
    * formatting
    
    Co-authored-by: Asa Hopkins <92234649+Asa-Hopkins@users.noreply.github.com>

--- src/basicTypes.cpp
+++ src/basicTypes.cpp
@@ -9,22 +9,25 @@ namespace BS {
 // eight steps per full rotation. Positive values are clockwise; negative
 // values are counterclockwise. E.g., rotate(4) returns a direction 90
 // degrees to the right.
+constexpr Compass NW = BS::Compass::NW; constexpr Compass N = BS::Compass::N;
+constexpr Compass NE = BS::Compass::NE; constexpr Compass E = BS::Compass::E;
+constexpr Compass SE = BS::Compass::SE; constexpr Compass S = BS::Compass::S;
+constexpr Compass SW = BS::Compass::SW; constexpr Compass W = BS::Compass::W;
+constexpr Compass C = BS::Compass::CENTER;
+
+const Dir rotations[72] = { SW, W, NW, N, NE, E, SE, S,
+                            S, SW, W, NW, N, NE, E, SE,
+                            SE, S, SW, W, NW, N, NE, E,
+                            W, NW, N, NE, E, SE, S, SW,
+                            C, C, C, C, C, C, C, C,
+                            E, SE, S, SW, W, NW, N, NE,
+                            NW, N, NE, E, SE, S, SW, W,
+                            N, NE, E, SE, S, SW, W, NW,
+                            NE, E, SE, S, SW, W, NW, N };
 
 Dir Dir::rotate(int n) const
 {
-    constexpr uint8_t rotateRight[9] { 3, 0, 1, 6, 4, 2, 7, 8, 5 };
-    constexpr uint8_t rotateLeft[9]  { 1, 2, 5, 0, 4, 8, 3, 6, 7 };
-    uint8_t n9 = asInt();
-    if (n < 0) {
-        while (n++ < 0) {
-            n9 = rotateLeft[n9];
-        }
-    } else if (n > 0) {
-        while (n-- > 0) {
-            n9 = rotateRight[n9];
-        }
-    }
-    return Dir{(Compass)n9};
+    return rotations[asInt() * 8 + (n & 7)];
 }
 
 
@@ -37,20 +40,32 @@ Dir Dir::rotate(int n) const
 
        Coord { (d%3) - 1, (trunc)(d/3) - 1  }
 
-       0 => -1, -1
-       1 =>  0, -1
-       2 =>  1, -1,
-       3 => -1,  0
-       4 =>  0,  0
-       5 =>  1   0
-       6 => -1,  1
-       7 =>  0,  1
-       8 =>  1,  1
+       0 => -1, -1  SW
+       1 =>  0, -1  S
+       2 =>  1, -1, SE
+       3 => -1,  0  W
+       4 =>  0,  0  CENTER
+       5 =>  1   0  E
+       6 => -1,  1  NW
+       7 =>  0,  1  N
+       8 =>  1,  1  NE
 */
+const Coord NormalizedCoords[9] = { 
+    Coord(-1,-1), // SW
+    Coord(0,-1),  // S
+    Coord(1,-1),  // SE
+    Coord(-1,0),  // W
+    Coord(0,0),   // CENTER
+    Coord(1,0),   // E
+    Coord(-1,1),  // NW
+    Coord(0,1),   // N
+    Coord(1,1)    // NE
+};
+
+
 Coord Dir::asNormalizedCoord() const
 {
-    const int d = asInt();
-    return Coord{(int16_t)((d % 3) - 1), (int16_t)((d / 3) - 1)};
+    return NormalizedCoords[asInt()];
 }
 
 
@@ -72,44 +87,28 @@ Coord Coord::normalize() const
 }
 
 
-// Calculate the angle, round to the nearest 360/8 degree slice, then
-// convert the slice to a Dir8Compass value.
+// Effectively, we want to check if a coordinate lies in a 45 degree region (22.5 degrees each side)
+// centered on each compass direction. By first rotating the system by 22.5 degrees clockwise
+// the boundaries to these regions become much easier to work with as they just align with the 8 axes.
+// (Thanks to @Asa-Hopkins for this optimization -- drm)
 Dir Coord::asDir() const
 {
-    if (x == 0 && y == 0) {
-        return Dir{Compass::CENTER};
-    }
+    // tanN/tanD is the best rational approximation to tan(22.5) under the constraint that
+    // tanN + tanD < 2**16 (to avoid overflows). We don't care about the scale of the result,
+    // only the ratio of the terms. The actual rotation is (22.5 - 1.5e-8) degrees, whilst
+    // the closest a pair of int16_t's come to any of these lines is 8e-8 degrees, so the result is exact
+    constexpr uint16_t tanN = 13860;
+    constexpr uint16_t tanD = 33461;
+    const Dir conversion[16] {  S, C, SW, N, SE, E, N,
+                                N, N, N, W, NW, N, NE, N, N};
 
-    const float TWO_PI = 3.1415927f * 2.0f;
-    float angle = std::atan2((float)y, (float)x);
+    const int32_t xp = x * tanD + y * tanN;
+    const int32_t yp = y * tanD - x * tanN;
 
-    if (angle < 0.0f) {
-        angle = 3.1415927f + (3.1415927f + angle);
-    }
-    //assert(angle >= 0.0 && angle <= TWO_PI);
-
-    angle += (TWO_PI / 16.0f);            // offset by half a slice
-    if (angle > TWO_PI) {
-        angle -= TWO_PI;
-    }
-    unsigned slice = (unsigned)(angle / (TWO_PI/8.0f));   // find which division it's in
-    /*
-        We have to convert slice values:
-
-            3  2  1
-            4     0
-            5  6  7
-
-        into Dir8Compass value:
-
-            6  7  8
-            3  4  5
-            0  1  2
-    */
-    const Compass dirconversion[8] {
-        Compass::E, Compass::NE, Compass::N, Compass::NW,
-        Compass::W, Compass::SW, Compass::S, Compass::SE };
-    return Dir{dirconversion[slice]};
+    // We can easily check which side of the four boundary lines
+    // the point now falls on, giving 16 cases, though only 9 are
+    // possible.
+    return conversion[(yp > 0) * 8 + (xp > 0) * 4 + (yp > xp) * 2 + (yp >= -xp)];
 }
 
 
@@ -128,21 +127,33 @@ Polar Coord::asPolar() const
 */
 Coord Polar::asCoord() const
 {
-    if (dir == Compass::CENTER) {
-        return Coord{0, 0};
-    }
+    // (Thanks to @Asa-Hopkins for this optimized function -- drm)
 
-    constexpr double S = (3.14159265359 * 2) / 8; // radians per slice
-    double compassToRadians[] { 5*S, 6*S, 7*S, 4*S, 0, 0*S, 3*S, 2*S, 1*S };
-//    uint8_t asint = dir.asInt();
-//    double sliceradians = compassToRadians[asint];
-//    double angle = std::cos(sliceradians);
-//    double angleXmag = mag * angle;
-//    double adj = angleXmag + 0.5;
-//    int16_t trunc = (int16_t)adj;
-    int16_t x = (int16_t)std::round(mag * std::cos(compassToRadians[dir.asInt()]));
-    int16_t y = (int16_t)std::round(mag * std::sin(compassToRadians[dir.asInt()]));
-    return Coord{x, y};
+    // 3037000500 is 1/sqrt(2) in 32.32 fixed point
+    constexpr int64_t coordMags[9] = {
+        3037000500,  // SW
+        1L << 32,    // S
+        3037000500,  // SE
+        1L << 32,    // W
+        0,           // CENTER
+        1L << 32,    // E
+        3037000500,  // NW
+        1L << 32,    // N
+        3037000500   // NE
+    };
+
+    int64_t len = coordMags[dir.asInt()] * mag;
+
+    // We need correct rounding, the idea here is to add/sub 1/2 (in fixed point)
+    // and truncate. We extend the sign of the magnitude with a cast,
+    // then shift those bits into the lower half, giving 0 for mag >= 0 and
+    // -1 for mag<0. An XOR with this copies the sign onto 1/2, to be exact
+    // we'd then also subtract it, but we don't need to be that precise.
+
+    int64_t temp = ((int64_t)mag >> 32) ^ ((1L << 31) - 1);
+    len = (len + temp) / (1L << 32); // Divide to make sure we get an arithmetic shift
+
+    return NormalizedCoords[dir.asInt()] * len;
 }
 
 
@@ -150,16 +161,12 @@ Coord Polar::asCoord() const
 // returns 1.0 if either vector is (0,0)
 float Coord::raySameness(Coord other) const
 {
-    float mag1 = std::sqrt(x * x + y * y);
-    float mag2 = std::sqrt(other.x * other.x + other.y * other.y);
-    if (mag1 == 0.0 || mag2 == 0.0) {
+    int64_t mag = ((int64_t)x * x + y * y) * (other.x * other.x + other.y * other.y);
+    if (mag == 0) {
         return 1.0; // anything is "same" as zero vector
     }
-    float dot = x * other.x + y * other.y;
-    float cos = dot / (mag1 * mag2);
-    assert(cos >= -1.0001 && cos <= 1.0001);
-    cos = std::min<float>(std::max<float>(cos, -1.0), 1.0); // clip
-    return cos;
+
+    return (x * other.x + y * other.y) / std::sqrt(mag);
 }
 
 
