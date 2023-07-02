cb92f636f1151932f8b0f9d434d65ca01eb9502b
timothy-r <tim.rodger@gmail.com>
2023-07-02 19:22:00 +0100

    Converts more tests to gtest

--- src/unitTestBasicTypes.cpp
+++ src/unitTestBasicTypes.cpp
@@ -204,80 +204,81 @@ bool unitTestBasicTypes()
     // assert(c1.asDir() == Compass::E);
 
     // .length()
-    assert(Coord(0, 0).length() == 0);
-    assert(Coord(0, 1).length() == 1);
-    assert(Coord(-1, 0).length() == 1);
-    assert(Coord(-1, -1).length() == 1); // round down
-    assert(Coord(22, 0).length() == 22);
-    assert(Coord(22, 22).length() == 31); // round down
-    assert(Coord(10, -10).length() == 14); // round down
-    assert(Coord(-310, 0).length() == 310);
+    // assert(Coord(0, 0).length() == 0);
+    // assert(Coord(0, 1).length() == 1);
+    // assert(Coord(-1, 0).length() == 1);
+    // assert(Coord(-1, -1).length() == 1); // round down
+    // assert(Coord(22, 0).length() == 22);
+    // assert(Coord(22, 22).length() == 31); // round down
+    // assert(Coord(10, -10).length() == 14); // round down
+    // assert(Coord(-310, 0).length() == 310);
 
     // .asPolar()
     Polar p1 = Coord(0, 0).asPolar();
-    assert(p1.mag == 0 && p1.dir == Compass::CENTER);
-    p1 = Coord(0, 1).asPolar();
-    assert(p1.mag == 1 && p1.dir == Compass::N);
-    p1 = Coord(-10, -10).asPolar();
-    assert(p1.mag == 14 && p1.dir == Compass::SW); // round down mag
-    p1 = Coord(100, 1).asPolar();
-    assert(p1.mag == 100 && p1.dir == Compass::E); // round down mag
+    // assert(p1.mag == 0 && p1.dir == Compass::CENTER);
+    // p1 = Coord(0, 1).asPolar();
+    // assert(p1.mag == 1 && p1.dir == Compass::N);
+    // p1 = Coord(-10, -10).asPolar();
+    // assert(p1.mag == 14 && p1.dir == Compass::SW); // round down mag
+    // p1 = Coord(100, 1).asPolar();
+    // assert(p1.mag == 100 && p1.dir == Compass::E); // round down mag
 
     // operator+(Coord), operator-(Coord)
-    Coord c1 = Coord(0, 0) + Coord(6, 8);
-    assert(c1.x == 6 && c1.y == 8);
-    c1 = Coord(-70, 20) + Coord(10, -10);
-    assert(c1.x == -60 && c1.y == 10);
-    c1 = Coord(-70, 20) - Coord(10, -10);
-    assert(c1.x == -80 && c1.y == 30);
+    // Coord c1 = Coord(0, 0) + Coord(6, 8);
+    // assert(c1.x == 6 && c1.y == 8);
+    // c1 = Coord(-70, 20) + Coord(10, -10);
+    // assert(c1.x == -60 && c1.y == 10);
+    // c1 = Coord(-70, 20) - Coord(10, -10);
+    // assert(c1.x == -80 && c1.y == 30);
 
     // operator*(int)
-    c1 = Coord(0, 0) * 1;
-    assert(c1.x == 0 && c1.y == 0);
-    c1 = Coord(1, 1) * -5;
-    assert(c1.x == -5 && c1.y == -5);
-    c1 = Coord(11, 5) * -5;
-    assert(c1.x == -55 && c1.y == -25);
+    // c1 = Coord(0, 0) * 1;
+    // assert(c1.x == 0 && c1.y == 0);
+    // c1 = Coord(1, 1) * -5;
+    // assert(c1.x == -5 && c1.y == -5);
+    // c1 = Coord(11, 5) * -5;
+    // assert(c1.x == -55 && c1.y == -25);
 
     // operator+(Dir), operator-(Dir)
-    c1 = Coord(0, 0);
-    Coord c2 = c1 + Dir(Compass::CENTER);
-    assert(c2.x == 0 && c2.y == 0);
-    c2 = c1 + Dir(Compass::E);
-    assert(c2.x == 1 && c2.y == 0);
-    c2 = c1 + Dir(Compass::W);
-    assert(c2.x == -1 && c2.y == 0);
-    c2 = c1 + Dir(Compass::SW);
-    assert(c2.x == -1 && c2.y == -1);
+    // c1 = Coord(0, 0);
+    // Coord c2 = c1 + Dir(Compass::CENTER);
+    // assert(c2.x == 0 && c2.y == 0);
+    // c2 = c1 + Dir(Compass::E);
+    // assert(c2.x == 1 && c2.y == 0);
+    // c2 = c1 + Dir(Compass::W);
+    // assert(c2.x == -1 && c2.y == 0);
+    // c2 = c1 + Dir(Compass::SW);
+    // assert(c2.x == -1 && c2.y == -1);
 
-    c2 = c1 - Dir(Compass::CENTER);
-    assert(c2.x == 0 && c2.y == 0);
-    c2 = c1 - Dir(Compass::E);
-    assert(c2.x == -1 && c2.y == 0);
-    c2 = c1 - Dir(Compass::W);
-    assert(c2.x == 1 && c2.y == 0);
-    c2 = c1 - Dir(Compass::SW);
-    assert(c2.x == 1 && c2.y == 1);
+    // c2 = c1 - Dir(Compass::CENTER);
+    // assert(c2.x == 0 && c2.y == 0);
+    // c2 = c1 - Dir(Compass::E);
+    // assert(c2.x == -1 && c2.y == 0);
+    // c2 = c1 - Dir(Compass::W);
+    // assert(c2.x == 1 && c2.y == 0);
+    // c2 = c1 - Dir(Compass::SW);
+    // assert(c2.x == 1 && c2.y == 1);
 
     // raySameness()
-    c1 = Coord { 0, 0 };
-    c2 = Coord { 10, 11 };
-    d1 = Compass::CENTER;
-    assert(c1.raySameness(c2) == 1.0); // special case - zero vector
-    assert(c2.raySameness(c1) == 1.0); // special case - zero vector
-    assert(c2.raySameness(d1) == 1.0); // special case - zero vector
-    c1 = c2;
-    assert(c1.raySameness(c2) == 1.0);
-    assert(areClosef(Coord(-10,-10).raySameness(Coord(10,10)), -1.0));
-    c1 = Coord{0,11};
-    c2 = Coord{20,0};
-    assert(areClosef(c1.raySameness(c2), 0.0));
-    assert(areClosef(c2.raySameness(c1), 0.0));
-    c1 = Coord{0,444};
-    c2 = Coord{113,113};
-    assert(areClosef(c1.raySameness(c2), 0.707106781));
-    c2 = Coord{113,-113};
-    assert(areClosef(c1.raySameness(c2), -0.707106781));
+    // Coord c1 = Coord { 0, 0 };
+    // Coord c2 = Coord { 10, 11 };
+    // d1 = Compass::CENTER;
+    // assert(c1.raySameness(c2) == 1.0); // special case - zero vector
+    // assert(c2.raySameness(c1) == 1.0); // special case - zero vector
+    // assert(c2.raySameness(d1) == 1.0); // special case - zero vector
+    // c1 = c2;
+    // assert(c1.raySameness(c2) == 1.0);
+    // assert(areClosef(Coord(-10,-10).raySameness(Coord(10,10)), -1.0));
+
+    // c1 = Coord{0,11};
+    // c2 = Coord{20,0};
+    // assert(areClosef(c1.raySameness(c2), 0.0));
+    // assert(areClosef(c2.raySameness(c1), 0.0));
+    // c1 = Coord{0,444};
+    // c2 = Coord{113,113};
+    // assert(areClosef(c1.raySameness(c2), 0.707106781));
+    // c2 = Coord{113,-113};
+    // assert(areClosef(c1.raySameness(c2), -0.707106781));
 
     // Polar
     // ctor from mag, dir
--- test/test_coord.cpp
+++ test/test_coord.cpp
@@ -4,6 +4,12 @@
 
 namespace BS {
 
+bool areClosef(float a, float b)
+{
+    return std::abs(a - b) < 0.0001;
+}
+
+
 TEST(CoordTests, TestDefaultCoord) {
     
     Coord c1 = Coord();
@@ -48,5 +54,138 @@ TEST(CoordTests, TestCoordNormalize) {
     EXPECT_EQ(-1, c3.x);
     EXPECT_EQ(0, c3.y);
     EXPECT_EQ(Dir(Compass::W), c3.asDir());
+}
+TEST(CoordTests, TestCoordLength) {
+
+    EXPECT_EQ(Coord(0, 0).length(), 0);
+    EXPECT_EQ(Coord(0, 1).length(), 1);
+    EXPECT_EQ(Coord(-1, 0).length(), 1);
+    EXPECT_EQ(Coord(-1, -1).length(), 1); // round down
+    EXPECT_EQ(Coord(22, 0).length(), 22);
+    EXPECT_EQ(Coord(22, 22).length(), 31); // round down
+    EXPECT_EQ(Coord(10, -10).length(), 14); // round down
+    EXPECT_EQ(Coord(-310, 0).length(), 310);
+}
+
+TEST(CoordTests, TestCoordAsPolar) {
+    Polar p1 = Coord(0, 0).asPolar();
+    EXPECT_EQ(p1.mag, 0);
+    EXPECT_EQ(Dir(Compass::CENTER), p1.dir);
+
+    p1 = Coord(0, 1).asPolar();
+    EXPECT_EQ(p1.mag, 1);
+    EXPECT_EQ(Dir(Compass::N), p1.dir);
+
+    p1 = Coord(-10, -10).asPolar();
+    EXPECT_EQ(p1.mag, 14);
+    EXPECT_EQ(Dir(Compass::SW), p1.dir); // round down mag
+
+    p1 = Coord(100, 1).asPolar();
+    EXPECT_EQ(p1.mag, 100);
+    EXPECT_EQ(Dir(Compass::E), p1.dir); // round down mag
+}
+
+TEST(CoordTests, TestCoordPlus) {
+    Coord c1 = Coord(0, 0) + Coord(6, 8);
+    EXPECT_EQ(c1.x, 6);
+    EXPECT_EQ(c1.y, 8);
+
+    c1 = Coord(-70, 20) + Coord(10, -10);
+    EXPECT_EQ(c1.x, -60);
+    EXPECT_EQ(c1.y, 10);
+}
+
+TEST(CoordTests, TestCoordMinus) {
+
+    Coord c1 = Coord(-70, 20) - Coord(10, -10);
+    EXPECT_EQ(c1.x, -80);
+    EXPECT_EQ(c1.y, 30);
+}
+
+TEST(CoordTests, TestCoordMultiply) {
+
+    Coord c1 = Coord(0, 0) * 1;
+    EXPECT_EQ(c1.x, 0);
+    EXPECT_EQ(c1.y, 0);
+
+    c1 = Coord(1, 1) * -5;
+    EXPECT_EQ(c1.x, -5);
+    EXPECT_EQ(c1.y, -5);
+
+    c1 = Coord(11, 5) * -5;
+    EXPECT_EQ(c1.x, -55);
+    EXPECT_EQ(c1.y, -25);
+}
+
+TEST(CoordTests, TestCoordPlusDir) {
+    Coord c1 = Coord(0, 0);
+    Coord c2 = c1 + Dir(Compass::CENTER);
+    EXPECT_EQ(c2.x, 0);
+    EXPECT_EQ(c2.y, 0);
+
+    c2 = c1 + Dir(Compass::E);
+    EXPECT_EQ(c2.x, 1);
+    EXPECT_EQ(c2.y, 0);
+    
+    c2 = c1 + Dir(Compass::W);
+    EXPECT_EQ(c2.x, -1);
+    EXPECT_EQ(c2.y, 0);
+    
+    c2 = c1 + Dir(Compass::SW);
+    EXPECT_EQ(c2.x, -1);
+    EXPECT_EQ(c2.y, -1);
+}
+
+TEST(CoordTests, TestCoordMinusDir) {
+    Coord c1 = Coord(0, 0);
+    Coord c2 = c1 - Dir(Compass::CENTER);
+    EXPECT_EQ(c2.x, 0);
+    EXPECT_EQ(c2.y, 0);
+
+    c2 = c1 - Dir(Compass::E);
+    EXPECT_EQ(c2.x, -1);
+    EXPECT_EQ(c2.y, 0);
+
+    c2 = c1 - Dir(Compass::W);
+    EXPECT_EQ(c2.x, 1);
+    EXPECT_EQ(c2.y, 0);
+
+    c2 = c1 - Dir(Compass::SW);
+    EXPECT_EQ(c2.x, 1);
+    EXPECT_EQ(c2.y, 1);
+}
+
+TEST(CoordTests, TestRaySameness) {
+
+    Coord c1 = Coord { 0, 0 };
+    Coord c2 = Coord { 10, 11 };
+    Dir d1 = Compass::CENTER;
+
+    EXPECT_EQ(c1.raySameness(c2), 1.0); // special case - zero vector
+    EXPECT_EQ(c2.raySameness(c1), 1.0); // special case - zero vector
+    EXPECT_EQ(c2.raySameness(d1), 1.0); // special case - zero vector
+}
+
+TEST(CoordTests, TestCoordRaySelfSameness) {
+
+    Coord c2 = Coord { 10, 11 };
+    Coord c1 = c2;
+    EXPECT_EQ(c1.raySameness(c2), 1.0);
+    EXPECT_TRUE(areClosef(Coord(-10,-10).raySameness(Coord(10,10)), -1.0));
+}
+
+TEST(CoordTests, TestAreCloseF) {
+
+    Coord c1 = Coord{0,11};
+    Coord c2 = Coord{20,0};
+    EXPECT_TRUE(areClosef(c1.raySameness(c2), 0.0));
+    EXPECT_TRUE(areClosef(c2.raySameness(c1), 0.0));
+
+    c1 = Coord{0,444};
+    c2 = Coord{113,113};
+    EXPECT_TRUE(areClosef(c1.raySameness(c2), 0.707106781));
+
+    c2 = Coord{113,-113};
+    EXPECT_TRUE(areClosef(c1.raySameness(c2), -0.707106781));
 }
 } 
